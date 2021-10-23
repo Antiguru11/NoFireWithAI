5aa94090f9922fd4de0ddea2ca0c943c6904cdba
Antiguru11 <antiguru110894@gmail.com>
2021-10-23 23:36:51 +0300

    make dev env

---
+++ startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p /root/nofirewithai
+
+git config --global user.name "Antiguru11"
+git config --global user.email "antiguru110894@gmail.com"
+git clone https://github.com/Antiguru11/NoFireWithAI.git /root/nofirewithai
+
+cd /root/nofirewithai
+/root/download.sh
